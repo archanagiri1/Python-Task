8e59f9138e45b71f6e2e4755707214fddf375f5d
Archana giri <babyboomba0@gmail.com>
2026-07-13 16:45:21 +0530

    Problem 3: Integer Division and Float Division

---
+++ Task3.py
@@ -0,0 +1,15 @@
+# Problem 3: Integer Division and Float Division
+# Problem Statement
+
+# Write a Python program that takes two integers as input from the user.
+
+# Print two lines:
+
+# The result of integer division (//).
+# The result of float division (/).
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
