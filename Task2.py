a88bca68d08d265fa0227e905c93847ac11bfaf8
Archana giri <babyboomba0@gmail.com>
2026-07-13 16:44:57 +0530

    Problem 2: Arithmetic Operations

---
+++ Task2.py
@@ -0,0 +1,18 @@
+# Problem 2: Arithmetic Operations
+# Problem Statement
+
+# Write a Python program that takes two integers as input from the user.
+
+# Your program should print:
+
+# The sum of the two numbers.
+# The difference of the two numbers (first number − second number).
+# The product of the two numbers.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
